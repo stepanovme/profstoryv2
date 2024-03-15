18ebd53b0502dd36e796379c3ddfe51b769321f8
Stepan Stepanov <st.stepanov57@gmail.com>
2024-03-15 14:16:35 +0300

    Add function add kompl-content for project incomplete

--- function/add_specpau.php
+++ function/add_specpau.php
@@ -1,12 +1,62 @@
 <?php
-// Получаем данные из POST-запроса
-$punic = $_POST['punic'];
-$anumb = $_POST['anumb'];
-$clnum = $_POST['clnum'];
-$formula = $_POST['formula'];
 
-// Дальнейшие действия с данными (например, добавление в базу данных или другие операции)
+session_start();
 
-// Возвращаем ответ (если необходимо)
-echo 'Данные успешно получены: PUNIC = ' . $punic . ', ANUMB = ' . $anumb . ', CLNUM = ' . $clnum . ', FORMULA = ' . $formula;
+if(!isset($_SESSION['userId'])){
+    header("Location: auth.php");
+    exit;
+}
+
+$userId = $_SESSION['userId'];
+
+$sql = "SELECT user.*, role.roleName
+        FROM user
+        INNER JOIN role ON user.roleId = role.roleId
+        WHERE user.userID = '$userId'";
+
+@include '../database/conn_mysql.php';
+
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    $row = $result->fetch_assoc();
+
+    $pathDB = $row['pathBD'];
+} else {
+    echo '0 results';
+}
+
+$punic = iconv('UTF-8', 'CP1251', $_POST['punic']);
+$anumb = iconv('UTF-8', 'CP1251', $_POST['anumb']);
+$clnum = iconv('UTF-8', 'CP1251', $_POST['clnum']);
+$formula = iconv('UTF-8', 'CP1251', $_POST['formula']);
+
+echo 'Данные успешно получены: PUNIC = '. $punic . ', ANUMB = ' . $anumb . ', CLNUM = ' . $clnum . ', FORMULA = ' . $formula;
+
+$host = $pathDB;
+$username = 'SYSDBA';
+$password = 'masterkey';
+
+global $ANUMB;
+
+try {
+    $dbh = new PDO("firebird:dbname=$host;charset=WIN1251", $username, $password);
+    
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // $sql = "INSERT INTO SPECPAU (PUNIC, ANUMB) VALUES ('$punic','$anumb')";
+    // SQL запрос для вставки данных в таблицу SPECPAU
+    $sql = "INSERT INTO SPECPAU (PUNIC, ANUMB, CLKK) VALUES ('$punic', '$anumb', '0')";
+
+    // $sql = "SELECT * FROM ARTIKLS WHERE ANUMB='$anumb'";
+
+    $sth = $dbh->query($sql);
+    
+    // while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+    //     $ANUMB = $row['ANUMB'];
+    // }
+    echo 'Данные добавлены';
+} catch (PDOException $e) {
+    echo "Ошибка соединения: " . $e->getMessage();
+}
 ?>
--- js/project-info-kompl.js
+++ js/project-info-kompl.js
@@ -63,6 +63,7 @@ $(document).ready(function() {
                 console.error(xhr.responseText);
             }
         });
+        location.reload();
     });
 });
 
